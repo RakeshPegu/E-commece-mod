20d1348800123d1c1dca1b4b3543869a9847faa4
RakeshPegu <rakeshpegu903@gmail.com>
2025-03-01 13:51:34 +0530

    the new client url has been added

--- client/my-react-app/src/lib/apiRequest.js
+++ client/my-react-app/src/lib/apiRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiRequest = new axios.create({
-    baseURL: "http://localhost:5500/api",
+    baseURL: "https://e-commece-mod-2.onrender.com/api",
     withCredentials:true
 })
 export default apiRequest;
