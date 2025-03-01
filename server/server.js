89bf89bf4592ae25fcc525eac8a5f4fbdc3b4515
RakeshPegu <rakeshpegu903@gmail.com>
2025-03-01 17:10:52 +0530

    there are some changes have been made

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ import productRoute from './routes/product.js'
 import userRoute from './routes/user.js'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 5000;
+// define the absolute path
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin: process.env.CLIENT_URL , credentials:true}))
+// serve static files from vite's build folder
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
 app.use('/api/auth', authRoute )
 app.use('/api/products',productRoute)
 
@@ -21,6 +26,20 @@ mongoose.connect(process.env.DATABASE_URL).then(()=>{
    
 }).catch((err)=>{
     console.log(err)
+})
+
+app.get('/api', async(req , res)=>{
+    try {
+        res.status(200).json({message:"the backend has been connected"})
+        
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong"})
+        
+    }
+})
+// catch-all route : Redirect all frontend request to 'index.html
+app.get('*', (req , res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 app.listen(port, ()=>{
     console.log(`the server is listening on port ${port}`)
