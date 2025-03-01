cb6c611e0bef564e2adc15a628d0f7deb17dd867
RakeshPegu <rakeshpegu903@gmail.com>
2025-03-01 14:42:12 +0530

    the production Client url has been added

--- client/my-react-app/src/lib/apiRequest.js
+++ client/my-react-app/src/lib/apiRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const apiRequest = new axios.create({
-    baseURL: "https://e-commece-mod-2.onrender.com/api",
+    baseURL: "https://e-commece-mod2.onrender.com/api",
     withCredentials:true
 })
 export default apiRequest;
